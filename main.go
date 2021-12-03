b076ea079eeddab41ec293ac9a64d4f583db5319
Tamal Saha <tamal@appscode.com>
2021-12-03 02:43:56 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -3,9 +3,55 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"time"
+)
+
+// A WeekendAdjustment specifies a day of the week (Sunday = 0, ...).
+type WeekendAdjustment int
+
+const (
+	NoChange WeekendAdjustment = iota
+	Before
+	After
 )
 
 func main() {
+	// adj := NoChange
+
+	now := time.Now().UTC().AddDate(0, 0, 1)
+	fmt.Println(now.Format(time.RFC3339))
+	d := now.Weekday()
+	fmt.Println(d)
+
+	fmt.Println(AdjustForWeekend(now, Before).Weekday())
+}
+
+func AdjustForWeekend(now time.Time, adj WeekendAdjustment) time.Time {
+	d := now.Weekday()
+
+	if d == time.Saturday {
+		switch adj {
+		case NoChange:
+			return now
+		case Before:
+			return now.AddDate(0, 0, -1)
+		case After:
+			return now.AddDate(0, 0, 2)
+		}
+	} else if d == time.Sunday {
+		switch adj {
+		case NoChange:
+			return now
+		case Before:
+			return now.AddDate(0, 0, -2)
+		case After:
+			return now.AddDate(0, 0, 1)
+		}
+	}
+	return now
+}
+
+func main__() {
 	var u url.URL
 	u.Scheme = "https" // "postgresql"
 	//u.User = url.UserPassword("{{username}}", "{{password}}")
